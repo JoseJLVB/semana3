278f296040256d303dbe6aca432adba83e252471
Jose Luis Valcarcel Bravo <josevalcarcel@entrearaucarias.com>
2022-04-12 13:50:49 -0400

    Proyecto terminado

--- src/semana3/Semana3.java
+++ src/semana3/Semana3.java
@@ -4,47 +4,47 @@
  */
 package semana3;
 
-import java.util.Scanner;
+import java.util.Scanner; //importamos clase java util para utilizar con metodo scanner
 
 /**
  *
  * @author Jose Valcarcel Bravo
  */
-public class Semana3 {
+public class Semana3 { //nombre de la clase
     
-public static void main (String[] args){
+public static void main (String[] args){ //esto permite que la clase sea ejecutable
 
-Scanner teclado = new Scanner (System.in);
+Scanner teclado = new Scanner (System.in); //declaramos el objeto tecladol para leer lo que se escribe en el teclado
 
-System.out.println("Ingrese Marca del Vehiculo: ");
+System.out.println("Ingrese Marca del Vehiculo: "); // se pide ingresar marca y se captura lo ingresado en el teclado
 
-String marca = teclado.next();
+String marca = teclado.next(); //se asigna a variable marca lo que se escribio en el teclado
 
-System.out.println("Ingrese Modelo del Vehiculo: ");
+System.out.println("Ingrese Modelo del Vehiculo: "); //se pide ingresar modelo y se captura lo ingresado en el teclado
 
-String modelo = teclado.next();
+String modelo = teclado.next(); //se asigna a variable modelo lo que se escribio en el teclado
 
-System.out.println("Ingrese Cilindrada del Vehiculo: ");
+System.out.println("Ingrese Cilindrada del Vehiculo: "); //se pide ingresar cilindrada y se captura lo ingresado en el teclado
 
-int cilindrada = teclado.nextInt();
+int cilindrada = teclado.nextInt(); //se asigna a variable cilindrada lo que se escribio en el teclado
 
-System.out.println("Ingrese Tipo de Combustible del Vehiculo: ");
+System.out.println("Ingrese Tipo de Combustible del Vehiculo: "); //se pide ingresar Tipo de combustible y se captura lo ingresado en el teclado
 
-String tipo = teclado.next();
+String tipo = teclado.next(); //se asigna a variable tipo lo que se escribio en el teclado
 
-System.out.println("Ingrese Cant. Pasajeros del Vehiculo: ");
+System.out.println("Ingrese Cant. Pasajeros del Vehiculo: "); //se pide ingresar cantidad de pasajeros y se captura lo ingresado en el teclado
 
-int pasajeros = teclado.nextInt();
+int pasajeros = teclado.nextInt(); //se asigna a variable pasajeros lo que se escribio en el teclado
 
 
 System.out.println("La Marca que ha ingresado es: " + marca +
         "\nEl Modelo que ha ingresado es: " + modelo+ 
         "\nLa Cilindrada que ha ingresado es: " + cilindrada +
         "\nEl tipo de combustrible es: " + tipo +
-        "\nTiene una capacidad de " + pasajeros + " pasajeros.");
+        "\nTiene una capacidad de " + pasajeros + " pasajeros."); // se imprimen en pantalla las variables y se muestra mensaje con la informacion ingresada.
 
 }//cierra método main
 
-}//cierra class
+}//cierra clase Semana3
     
 
